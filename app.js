80518fbe752e87165453e8d5dd56aea695590e8d
UlfetKacay <ukacay87@gmail.com>
2021-07-18 18:00:30 +0300

    updated

--- app.js
+++ app.js
@@ -26,23 +26,23 @@ const number8 = document.querySelector(".number-8");
 const number9 = document.querySelector(".number-9");
 
 const operatorsArray = [
-    additionButton,
-    subtractionButton,
-    multiplicationButton,
-    divisionButton,
+  additionButton,
+  subtractionButton,
+  multiplicationButton,
+  divisionButton,
 ];
 
 const numbersArray = [
-    number0,
-    number1,
-    number2,
-    number3,
-    number4,
-    number5,
-    number6,
-    number7,
-    number8,
-    number9,
+  number0,
+  number1,
+  number2,
+  number3,
+  number4,
+  number5,
+  number6,
+  number7,
+  number8,
+  number9,
 ];
 let previousOperand = "";
 let currentOperand = "";
@@ -51,136 +51,135 @@ let temporaryOperand = "";
 
 // Functions
 function DisplayNumbers() {
-    if (operation) {
-        previousElement.innerHTML = `${previousOperand} ${operation}`;
-    } else {
-        previousElement.innerHTML = previousOperand;
-    }
-    currentElement.innerHTML = currentOperand;
+  if (operation) {
+    previousElement.innerHTML = `${previousOperand} ${operation}`;
+  } else {
+    previousElement.innerHTML = previousOperand;
+  }
+  currentElement.innerHTML = currentOperand;
 }
 
 function AppendNumber(number) {
-    if (number === "." && currentOperand.includes(".")) {
-        return; // koşul True olduğunda fonksiyonu çalıştırmaz.Birden fazla "." eklenmesi engellendi.
-    }
-    if (number === 0 && currentOperand === "0") {
-        return; // koşul True olduğunda fonksiyonu çalıştırmaz.Birden fazla "0" eklenmesi engellendi.
-    }
-    if (currentOperand.length > 6) {
-        return; // Ekrana sığması için currentOperand uzunluğu sınırlandırıldı.
-    }
-    currentOperand += number;
-    DisplayNumbers();
+  if (number === "." && currentOperand.includes(".")) {
+    return; // koşul True olduğunda fonksiyonu çalıştırmaz.Birden fazla "." eklenmesi engellendi.
+  }
+  if (number === 0 && currentOperand === "0") {
+    return; // koşul True olduğunda fonksiyonu çalıştırmaz.Birden fazla "0" eklenmesi engellendi.
+  }
+  if (currentOperand.length > 6) {
+    return; // Ekrana sığması için currentOperand uzunluğu sınırlandırıldı.
+  }
+  currentOperand += number;
+  DisplayNumbers();
 }
 
 function ChooseOperation(selectedOperation) {
-    if (temporaryOperand) {
-        previousOperand = temporaryOperand.toString();
-        currentOperand = "";
-        temporaryOperand = "";
-        operation = selectedOperation;
-        DisplayNumbers();
-        return;
-    }
-    // seçilen operatörü değiştirir.
-    if (operation) {
-        operation = selectedOperation;
-        DisplayNumbers();
-        return;
-
-    }
-    operation = selectedOperation;
-    previousOperand = currentOperand;
+  if (temporaryOperand) {
+    previousOperand = temporaryOperand.toString();
     currentOperand = "";
+    temporaryOperand = "";
+    operation = selectedOperation;
     DisplayNumbers();
+    return;
+  }
+  // seçilen operatörü değiştirir.
+  if (operation) {
+    operation = selectedOperation;
+    DisplayNumbers();
+    return;
+  }
+  operation = selectedOperation;
+  previousOperand = currentOperand;
+  currentOperand = "";
+  DisplayNumbers();
 }
 
 function Compute() {
-    let computation;
-    const previous = parseFloat(previousOperand);
-    const current = parseFloat(currentOperand);
-    if (!operation) return;
-    if (isNaN(previous) || isNaN(current)) return;
+  let computation;
+  const previous = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (!operation) return;
+  if (isNaN(previous) || isNaN(current)) return;
 
-    switch (operation) {
-        case "+":
-            computation = previous + current;
-            break;
-        case "-":
-            computation = previous - current;
-            break;
-        case "x":
-            computation = previous * current;
-            break;
-        case "/":
-            if (previous % current === 0) {
-                computation = previous / current;
-            } else {
-                computation = (previous / current).toFixed(2);
-            }
-            break;
-        default:
-            break;
-    }
+  switch (operation) {
+    case "+":
+      computation = previous + current;
+      break;
+    case "-":
+      computation = previous - current;
+      break;
+    case "x":
+      computation = previous * current;
+      break;
+    case "/":
+      if (previous % current === 0) {
+        computation = previous / current;
+      } else {
+        computation = (previous / current).toFixed(2);
+      }
+      break;
+    default:
+      break;
+  }
 
-    if (isNaN(computation)) return;
+  if (isNaN(computation)) return;
 
-    currentOperand = computation;
-    previousOperand = "";
-    operation = "";
-    DisplayNumbers();
-    temporaryOperand = currentOperand;
-    currentOperand = "";
+  currentOperand = computation;
+  previousOperand = "";
+  operation = "";
+  DisplayNumbers();
+  temporaryOperand = currentOperand;
+  currentOperand = "";
 }
 
 function AllClear() {
-    operation = "";
-    previousOperand = "";
-    currentOperand = "";
-    DisplayNumbers();
+  operation = "";
+  previousOperand = "";
+  currentOperand = "";
+  DisplayNumbers();
 }
 
 function PlusMinus() {
-    currentOperand = currentOperand * -1;
-    DisplayNumbers();
+  currentOperand = currentOperand * -1;
+  DisplayNumbers();
 }
 
 function Delete() {
-    currentOperand = currentOperand.slice(0, currentOperand.length - 1);
-    DisplayNumbers();
+  currentOperand = currentOperand.slice(0, currentOperand.length - 1);
+  DisplayNumbers();
 }
 
 // Add event listener to operator buttons
 for (let j = 0; j < operatorsArray.length; j++) {
-    const operator = operatorsArray[j];
-    operator.addEventListener("click", () => {
-        ChooseOperation(operatorsArray[j].textContent);
-    });
+  const operator = operatorsArray[j];
+  operator.addEventListener("click", () => {
+    ChooseOperation(operatorsArray[j].textContent);
+  });
 }
 
 // Add event listener to top buttons
 acButton.addEventListener("click", () => {
-    AllClear();
+  AllClear();
 });
 equalsButton.addEventListener("click", () => {
-    Compute();
+  Compute();
 });
 pmButton.addEventListener("click", () => {
-    PlusMinus();
+  PlusMinus();
 });
 deleteButton.addEventListener("click", () => {
-    Delete();
+  Delete();
 });
 
 // Add event listener to number buttons
 
 for (let i = 0; i < numbersArray.length; i++) {
-    const number = numbersArray[i];
-    number.addEventListener("click", () => {
-        AppendNumber(i);
-        temporaryOperand = "";
-    });
+  const number = numbersArray[i];
+  number.addEventListener("click", () => {
+    AppendNumber(i);
+    temporaryOperand = "";
+  });
 }
 decimalButton.addEventListener("click", () => {
-    AppendNumber(".");
+  AppendNumber(".");
 });
